c714ebf2281e002cb4e5ab3e2d75a806f706bd23
VIMER <top.michan@mail.ru>
2026-03-04 22:22:27 +0500

    feat(webrtc): реализация серверной обработки сигнализации для звонков
    - Добавлено хранилище onlineMap для отслеживания онлайн-пользователей (задел для будущего функционала)
    - Реализована обработка WebRTC-событий на сервере через сокеты:
      - Событие "call-user" - инициирование звонка:
        - Перенаправляет SDP-предложение (offer) в указанный чат
        - Отправляет событие "offer-call" всем участникам комнаты чата
      - Событие "offer-answer" - ответ на звонок:
        - Перенаправляет SDP-ответ (answer) конкретному пользователю
        - Отправляет событие "answer-made" по ID сокета получателя
      - Событие "ice-candidate" - обмен ICE-кандидатами:
        - Транслирует ICE-кандидаты всем участникам чата
        - Отправляет событие "ice-candidate" в комнату чата
    - Улучшена маршрутизация сигнальных сообщений:
      - Использование комнат чатов (chat:${id}) для групповой рассылки
      - Возможность таргетированной отправки конкретным пользователям по socket.id

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -2,6 +2,8 @@ import { Server, Socket } from "socket.io";
 import { CustomRequestSocketIO } from "types/customRequestType.js";
 import chatsService from "./../service/chatsService.js";
 
+let onlineMap = new Map();
+
 export const onConnection = async (io: Server, socket: CustomRequestSocketIO) => {
   const userId = socket.userId;
 
@@ -14,6 +16,28 @@ export const onConnection = async (io: Server, socket: CustomRequestSocketIO) =>
     }
   }
   socket.join(`user:${userId}`);
+
+  socket.on("call-user", (data) => {
+    console.log(data);
+    socket.to(`chat:${data.to}`).emit("offer-call", {
+      sdp: data.sdp,
+      from: socket.id,
+      chatID: data.to,
+    });
+  });
+  socket.on("offer-answer", (data) => {
+    console.log(data);
+    socket.to(data.to).emit("answer-made", {
+      sdp: data.sdp,
+      from: socket.id,
+    });
+  });
+  socket.on("ice-candidate", (data) => {
+    socket.to(`chat:${data.to}`).emit("ice-candidate", {
+      candidate: data.candidate,
+      from: socket.id,
+    });
+  });
 
   socket.on("disconnect", () => {
     console.log(`User disconnected: ${userId}`);
